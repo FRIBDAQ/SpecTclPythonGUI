6b47a124270f76c83a01a09f99c8a12ab83e4883
Ron Fox <fox@frib.msu.edu>
2025-11-18 08:55:53 -0500

    Issue #21 - superimpose fits on waveform plots
    Got the superposition done, need to figure out how to add a legend.

--- waveformview.py
+++ waveformview.py
@@ -182,11 +182,15 @@ class PlotWidget(FigureCanvasQTAgg):
         Encapsulates the magic needed to use matplottlib to plot a waveform.
         Methods:
            plot - erases any prior waveform plot and puts in new points.
+           add_fit - adds a fitline.
            set_title - set plot title
     '''       
+    fit_colors = ['red', 'green', 'blue', 'magenta', 'cyan', 'yellow ',
+        'dark green', 'brown', 'hot pink']
     def __init__(self, parent=None):
         self._fig=Figure()
         self._axis=None
+        self._next_color = 0
         super().__init__(self._fig)
         
     def plot(self, samples):
@@ -196,6 +200,9 @@ class PlotWidget(FigureCanvasQTAgg):
         '''
         if self._axis is not None:
             self._fig.delaxes(self._axis)
+            # Kill off any fit axes.
+           
+        self._next_color = 0   # Reset the next fit color.
         
         # make a new axis and plot in it.
         # note we need to make the xpts. the ypts are the samples.
@@ -204,12 +211,28 @@ class PlotWidget(FigureCanvasQTAgg):
         
         self._axis.plot(samples)
         self.draw()
+    def add_fit(self, name, points):
+        ''' Add a fitline to the plot.  The waveform must have been
+            drawn first with plot().
+            name - name of the fit.
+            points - fit points.
+        '''
+        
+        self._axis.plot(points, color=self._color())
+        self.draw()
+        
     def set_title(self, text):
         '''
            Set figure title text (e.g. waveform name.)
         '''
         self._fig.suptitle(text)
-    
+    def _color(self): 
+        # Assign a color:
+        result = self.fit_colors[self._next_color]
+        self._next_color += 1
+        if self._next_color >= len(self.fit_colors):
+            self._next_color = 0            # cycle if needed.
+        return result
 class WaveformPlot(QWidget) :
     '''
         This widget provides a plot widget for viewing waveforms.  It consists of
@@ -251,8 +274,9 @@ class WaveformPlot(QWidget) :
         '''
         self._plot.plot(samples)
         self._plot.set_title(name)
+        
     def add_fit(self, name, points):
-        print("adding fit", name)     # Need to add to the plot object.
+        self._plot.add_fit(name, points)    # Need to add to the plot object.
         
     def enable(self):
         ''' turn on the refresh button: 
