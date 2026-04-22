56e39c1a0be1427806a6c7f6fe9735bc0eef9cc4
Ron Fox <fox@frib.msu.edu>
2026-04-22 09:33:10 -0400

    Issue #23 Support vectors.
    The vector MVC/editor are tested and working.

--- vectorparams.py
+++ vectorparams.py
@@ -21,7 +21,8 @@
                      
 '''
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QAbstractItemView,  QTableView
+from PyQt5.QtWidgets import (QTableView, QWidget, QLineEdit, QFrame,
+    QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout)
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.Qt import *
 
@@ -53,7 +54,7 @@ class VectorParameterModel(QStandardItemModel) :
       
       # Clear the model if it has data:
       if self.rowCount() > 0:
-        self.removeROws(0, self.rowCount())
+        self.removeRows(0, self.rowCount())
       
       #  Stuff the row:
       
@@ -136,6 +137,151 @@ class VectorTableView(QTableView) :
   #    happened.
   def _onDoubleClick(self, index) : 
     self.onDoubleClick.emit(index.row())
+        
+class VectorEditor(QWidget) :
+  '''
+    This widget allows users to edit a vector.  Visual appearance is:
+    +--------------------------------------+
+    |   Name    Low   High  Bins   Units   | 
+    |  <name>  [low]  [hi ] [bins] [units] |
+    +--------------------------------------+
+    |    [ OK ]  [Cancel]                  |
+    +--------------------------------------+
+    
+    The <name> is a non-editable name of the vector, the items in
+    square brackets are entries that are editable.
+    
+    Signals:
+      ok     - The OK button was clicked.
+      cancel - the Cancel button was clicked.
+  '''
+  ok     = pyqtSignal()
+  cancel = pyqtSignal()
+  
+  def __init__(self, *args) :
+    print("Initializing editor")
+    super().__init__(*args)
+    
+    
+    self._widgetlayout = QVBoxLayout()  
+    self.setLayout(self._widgetlayout)
+    
+    # The top widget contains the labels and edits and
+    # live in a frame to support the line between them and
+    # the buttons:
+  
+      
+    
+    self._editor = QFrame(self)
+    self._editor.setFrameShape(QFrame.Box)
+    self._editor.setLineWidth(2)
+  
+    self._editorlayout = QGridLayout()
+    
+    
+    # The labels:
+    self._editor_nameLabel = QLabel("Name", self._editor)
+    self._editor_lowLabel  = QLabel("Low", self._editor)
+    self._editor_highLabel = QLabel("High", self._editor)
+    self._editor_binsLabel = QLabel("Bins", self._editor)
+    self._editor_unitsLabel= QLabel("UNits", self._editor)
+    self._editorlayout.addWidget(self._editor_nameLabel, 0,0)
+    self._editorlayout.addWidget(self._editor_lowLabel,  0,1)
+    self._editorlayout.addWidget(self._editor_highLabel, 0,2)
+    self._editorlayout.addWidget(self._editor_binsLabel, 0,3)
+    self._editorlayout.addWidget(self._editor_unitsLabel, 0,4)
+    
+    # THe editor itself, note that the name is not editable and
+    # therefore is a label. THe others are lineentries.
+  
+    self._editor_name = QLabel(self._editor)   # We don't know the contents yet.
+    self._editor_low  = QLineEdit(self._editor)  
+    self._editor_high = QLineEdit(self._editor)
+    self._editor_bins = QLineEdit(self._editor)
+    self._editor_units= QLineEdit(self._editor)
+    self._editorlayout.addWidget(self._editor_name,  1,0)
+    self._editorlayout.addWidget(self._editor_low,   1,1)
+    self._editorlayout.addWidget(self._editor_high,  1,2)
+    self._editorlayout.addWidget(self._editor_bins,  1,3)
+    self._editorlayout.addWidget(self._editor_units, 1,4)
+    
+    self._editor.setLayout(self._editorlayout)
+    
+    # The widget is a vertical box with the editor on top
+    # and a horizontal row of buttons on the bottom.
+    
+    
+    self._widgetlayout.addWidget(self._editor)
+    self.setLayout(self._widgetlayout)
+    
+    self.setLayout(self._widgetlayout)
+    
+    
+    # Now the buttons.
+    
+  
+    buttonlayout = QHBoxLayout()
+    self._ok     = QPushButton("Ok", self)
+    self._cancel = QPushButton("Cancel", self)
+    buttonlayout.addWidget(self._ok)
+    buttonlayout.addWidget(self._cancel)
+    self._widgetlayout.addLayout(buttonlayout) 
+    
+    # map signals  for the buttons.
+    
+    self._ok.clicked.connect(self.ok)
+    self._cancel.clicked.connect(self.cancel)
+
+  def load(self, vector):
+    '''
+      retrieves the editor contents with the data from 
+      the vector description map passed.
+      
+      Parameters:
+         vector - a map with the indices (all values are text).
+          * 'name' - name of the vector.
+          * 'low'  - advisory low limit
+          * 'high' - advisory high limit.
+          * 'bins' - advisory bin count.
+          * 'units' - units of measure.
+          
+    '''
+    self._editor_name.setText(vector['name'])
+    self._editor_low.setText(vector['low'])
+    self._editor_high.setText(vector['high'])
+    self._editor_bins.setText(vector['bins'])
+    self._editor_units.setText(vector['units'])
+    
+  def get(self):
+    '''
+      Returns:
+         A description of the vector that is currently in the editor.
+         It is up to the caller to do any validation (e.g. low, high, units integers
+         low < high?) etc 
+         The return value is a dict withthe keys described in load.
+         Note that the values are all textual.
+         
+    '''
+    name  = self._editor_name.text()
+    low   = self._editor_low.text()
+    high  = self._editor_high.text()
+    bins  = self._editor_bins.text()
+    units = self._editor_units.text()
+    
+    return {
+      'name' : name, 'low' : low, 'high' : high,
+      'bins' : bins, 'units' : units
+    }
+    
+  def clear(self):  
+    '''
+      clear the contents of the editor
+    '''
+    self._editor_name.setText('')
+    self._editor_low.setText('')
+    self._editor_high.setText('')
+    self._editor_bins.setText('')
+    self._editor_units.setText('')
    
 # Test code here:
 
@@ -146,10 +292,31 @@ if __name__ == "__main__":
   from PyQt5.QtWidgets import QApplication, QMainWindow
   
   def selectItem(row) :
-    print("Selected ", row)
+    
     info = model.get(row)
-    print(info)
+    editor.load(info)
+    
+  def ok() :
+    info = editor.get()
+    name = info['name']
+    low  = int(info['low'])
+    high = int(info['high'])
+    bins = int(info['bins'])
+    units = info['units']
+    
+    c.vector_setlow(name, low)
+    c.vector_sethigh(name, high)
+    c.vector_setbins(name, bins)
+    c.vector_setunits(name, units)
+    
+    editor.clear()
+    
+    model.load()     #That updates the table.
   
+  
+  def cancel() :
+    editor.clear()
+      
   c = rc({'host': 'localhost', 'port': 8000})
   app = QApplication([])
   win = QMainWindow()
@@ -157,10 +324,22 @@ if __name__ == "__main__":
   model = VectorParameterModel(c)
   model.load()
   view = VectorTableView(model)
-  
   view.onDoubleClick.connect(selectItem)
-  win.setCentralWidget(view)
+  
+  editor = VectorEditor()
+  editor.ok.connect(ok)
+  editor.cancel.connect(cancel)
+  
+  main = QWidget()
+  layout = QVBoxLayout()
+  main.setLayout(layout)
+  layout.addWidget(view)
+  layout.addWidget(editor)
+  
+  
+  win.setCentralWidget(main)
   win.show()
+  
   app.exec()
   
   
